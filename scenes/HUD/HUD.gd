9e1e7a7263c5037bab1ec8dc0a9cdb11503c5968
Ryan Roden-Corrent <git@rrc.codes>
2025-08-10 20:08:39 -0400

    Improve HUD crosshair.

--- scenes/HUD/HUD.gd
+++ scenes/HUD/HUD.gd
@@ -2,16 +2,16 @@ extends Control
 
 @onready var player: Player
 
-@onready var throw_power_bar: ProgressBar = $ThrowPowerBar
 @onready var tunnel_vision: Control = $TunnelVision
 @onready var game_over: Control = $GameOver
+
+var crosshair_radius := 1.0
 
 func _ready() -> void:
 	player = get_parent()
 	assert(player, "HUD cannot find player")
 
 func _process(delta: float) -> void:
-	throw_power_bar.value = player.throw_charge
 	tunnel_vision.modulate.a = player.panic
 	if player.panic >= 1.0:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -19,3 +19,15 @@ func _process(delta: float) -> void:
 			player.drop()
 		game_over.visible = true
 		game_over.modulate.a = move_toward(game_over.modulate.a, 1.0, delta)
+
+	# expand the crosshair if we can grab an object
+	var target_radius := 1.0
+	if player.grab_ray.is_colliding() and not player.held_object:
+		target_radius = 8.0
+	crosshair_radius = lerp(crosshair_radius, target_radius, delta * 12.0)
+	queue_redraw()
+
+func _draw() -> void:
+	var center := size / 2.0
+	draw_circle(center, crosshair_radius, Color.WHITE, false, 2.0, true)
+	draw_arc(center, 16.0, 0, 2 * PI * player.throw_charge, 128, Color.GREEN.lerp(Color.RED, player.throw_charge), 4.0, true)
